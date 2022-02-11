defefc04a146f03bff2390f211a7ab4ec903904d
tonydavis629 <acdavis629@gmail.com>
2022-02-11 16:46:47 -0500

    fix 0 dem, working

--- generate_data.py
+++ generate_data.py
@@ -19,7 +19,7 @@ def generate_vrp_data(dataset_size, vrp_size):
         np.random.uniform(size=(dataset_size, 2)).tolist(),  # Depot location
         np.random.uniform(size=(dataset_size, vrp_size, 2)).tolist(),  # Node locations
         # np.random.randint(1, 10, size=(dataset_size, vrp_size)).tolist(),  # Demand, uniform integer 1 ... 9
-        np.zeros(dataset_size,vrp_size).tolist(),
+        np.zeros((dataset_size,vrp_size)).tolist(),
         np.full(dataset_size, CAPACITIES[vrp_size]).tolist()  # Capacity, same for whole dataset
     ))
 
